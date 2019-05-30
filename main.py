fa4ec5726b8be8fc1a897c3dedeb2b394c5fdb98
Theo <theolpmodesto@gmail.com>
2019-05-30 18:53:47 -0300

    fit main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import  Flask
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
+@app.route('/')
+def index():
+        return "hello world"
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
