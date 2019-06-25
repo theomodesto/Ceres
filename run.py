a65564b563b4b71b93d520e656a7c8d46c8e37bc
Patinho <brunompatinho@gmail.com>
2019-06-25 16:34:37 -0300

    BETA 1.0

--- run.py
+++ run.py
@@ -10,13 +10,13 @@ app = Flask(__name__)
 def Home():
     return render_template('home.html')
 
-@app.route('/Plantas/<lat>/<lon>')
+@app.route('/Plantas/<lat>/<lon>', methods=['POST','GET'])
 def previsao(lat,lon):
-    print(lat,lon)
+    print("As cordenadas passadas foram:" + lat,lon)
     previsoes = previsaoFinal(lat,lon)
     return render_template('plantas.html', result=previsoes)
 
-@app.route('/Passos/<id>',methods=['GET'])
+@app.route('/Passos/<id>',methods=['POST','GET'])
 def Plante(id):
     planta = retornaPlantasPorId(id)
     return render_template('passos.html',passos=planta["Passos"])
