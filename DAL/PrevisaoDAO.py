2d8795f74944cfb08e747e1dcc8eb52e627d74eb
Theo <ti@queencs.com.br>
2019-06-25 14:20:58 -0300

    lista plantas

---
+++ DAL/PrevisaoDAO.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from Util.PowerLarcAPI import *
+from IA.Previsao import *
+from DAL.PlantasDAO import *
+
+def previsaoFinal(lat,lon):
+    previsoes = []
+    ano = ((datetime.now().year) - 1)
+    Dados = GetDadosAPI(lat, lon, ano)
+    for key in Dados:
+        previsao = Previsao(Dados[key])
+        for prev in previsao:
+            print(prev, key)
+            planta = retornaPlantasPorId(prev['IdPlanta'])
+            previsoes.append([planta, prev['Probabilidade'], key.lower()])
+    return previsoes
