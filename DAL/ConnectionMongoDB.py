ee19ffca4d197007aa2ed9020e7b0e95676f7e59
theomodesto <33163645+theomodesto@users.noreply.github.com>
2019-09-05 11:12:09 -0300

    Update ConnectionMongoDB.py

--- DAL/ConnectionMongoDB.py
+++ DAL/ConnectionMongoDB.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 def Connection(Collection):
-    #client = MongoClient('localhost',27017)
-    client = MongoClient("mongodb://Ceres:Ceres1234@ceres-shard-00-00-vol56.mongodb.net:27017,ceres-shard-00-01-vol56.mongodb.net:27017,ceres-shard-00-02-vol56.mongodb.net:27017/test?ssl=true&replicaSet=Ceres-shard-0&authSource=admin&retryWrites=true&w=majority")
+    client = MongoClient('localhost',27017)
+    #client = MongoClient("")
 
     #DATABASE
     db = client.test
